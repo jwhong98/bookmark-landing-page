95846329a48db82d7faf9ff0aaf92c132a841c6f
jwhong <jwhong98@gmail.com>
2022-03-08 17:16:34 -0800

    added Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,32 @@
 import React from "react";
+import classes from "./Hero.module.css";
+import img from "../../images/illustration-hero.svg";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className={classes.hero}>
+      <div className={classes.heroContainer}>
+        <div className={classes.heroText}>
+          <h1>a simple bookmark manager</h1>
+          <p>
+            A clean and simple interface to organize your favourite websites.
+            Open a new browser tab and see your sites load instantly. Try for
+            free.
+          </p>
+          <div className={classes.buttonContainer}>
+            <button>Get it on Chrome</button>
+            <button>Get it on Firefox</button>
+          </div>
+        </div>
+
+        {/* either use image container or just the img tag */}
+        <div className={classes.imgContainer}>
+          <img src={img} alt="" />
+        </div>
+      </div>
+      <div className={classes.blueBar}></div>
+    </section>
+  );
 };
 
 export default Hero;
